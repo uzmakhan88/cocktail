1f81435ec41c5f2a7c44418913ce42c8bbe67345
Shubh1622 <shubhamdutta1622@gmail.com>
2023-08-16 10:20:08 +0530

    coctail-app
    by me

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <div className="mt-4 bg-dark text-white">
       <div className="container p-4 text-center">
-        <h2>Design And Developed By : uzma khan </h2>
-        <h6>All Right Reserved by &copy; uzma khan</h6>
+        <h2>Design And Developed By : SHUBHAM DUTTA </h2>
+        <h6>All Right Reserved by &copy; SHUBHAM DUTTA</h6>
       </div>
     </div>
   );
